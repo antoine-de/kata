6a59372983dd1544befbb7ed490ea7737af20839
antoine-de <antoine.desbordes@gmail.com>
2021-08-18 16:13:45 +0200

    add right file and smal corner case correction

--- anagram/src/bin/basis_solution.rs
+++ anagram/src/bin/basis_solution.rs
@@ -1,33 +1,36 @@
 fn read_words() -> Vec<String> {
-    let content = std::fs::read_to_string("./wordlist.txt").expect("unable to read word list file");
-    content
-        .split('\n')
-        .skip(1)
-        .map(|s| s.split(' '))
-        .flatten()
-        .filter(|s| !s.is_empty())
-        .map(|s| s.to_string())
-        .collect()
+    let content =
+        std::fs::read_to_string("./wordlist2.txt").expect("unable to read word list file");
+    content.split('\n').map(|s| s.to_string()).collect()
 }
+//fn read_words() -> Vec<String> {
+//    let content = std::fs::read_to_string("./wordlist.txt").expect("unable to read word list file");
+//    content
+//        .split('\n')
+//        .skip(1)
+//        .map(|s| s.split(' '))
+//        .flatten()
+//        .filter(|s| !s.is_empty())
+//        .map(|s| s.to_string())
+//        .collect()
+//}
 
 fn get_char_value(c: char, base_word: &str) -> Option<usize> {
     base_word
         .char_indices()
         .find(|(_i, wc)| c == *wc)
-        .map(|(i, _wc)| i + 1) // we want the first element to have value 1, not 0, else it would not be taken into account in the basis
+        .map(|(i, _wc)| i)
 }
 
 fn get_word_value(word: &str, base_word: &str) -> Option<usize> {
     word.chars()
-        .map(|c| get_char_value(c, base_word).map(|v| v.pow(base_word.len() as u32)))
+        .map(|c| get_char_value(c, base_word).map(|v| base_word.len().pow(v as u32)))
         .sum()
 }
 
 fn generate_anagrams(initial_word: &str, words_list: &Vec<String>) -> Vec<String> {
     let initial_word_value = get_word_value(initial_word, initial_word)
         .expect("by construction, all letter should be there");
-
-    //println!("init val = {}", initial_word_value);
 
     let words_values = words_list
         .into_iter()
@@ -36,33 +39,22 @@ fn generate_anagrams(initial_word: &str, words_list: &Vec<String>) -> Vec<String
 
     let mut anagrams = Vec::new();
     for (i1, (v1, w1)) in words_values.iter().enumerate() {
-        for (v2, w2) in words_values[i1+1..].iter() {
-            if initial_word_value == v1 + v2 {
+        for (v2, w2) in &words_values[i1 + 1..] {
+            if initial_word_value == v1 + v2 && w1.len() + w2.len() == initial_word.len() {
                 anagrams.push(format!("{} {}", w1, w2));
             }
         }
     }
     anagrams
-    
-    /*
-    iterator version, with itertools:
-    use itertools::Itertools;
-    words_values
-        .iter()
-        .combinations(2)
-        .filter(|words| initial_word_value == words.iter().map(|(v, _w)| v).sum())
-        .map(|words| words.iter().map(|(_v, w)| w).join(" "))
-        .collect()
-        */
 }
 
 fn main() {
-    let initial_word = "platvisibleo";
+    let initial_word = "documenting";
     let words = read_words();
     //println!("words: {:?}", words);
     println!("words: {}", words.len());
 
-    for i in 0..100000 {
+    for i in 0..100 {
         let anagrams = generate_anagrams(initial_word, &words);
 
         if i == 0 {
@@ -120,4 +112,13 @@ fn word_without_letters_in_base_word() {
         ],
     );
     assert_eq!(anagrams, vec!["ab c", "bc a", "ac b"])
+}
+
+#[test]
+fn corner_case() {
+    let anagrams = generate_anagrams(
+        "ab",
+        &vec!["aa".to_string(), "a".to_string(), "b".to_string()],
+    );
+    assert_eq!(anagrams, vec!["a b"]) // aa a should not be a valid anagram
 }
