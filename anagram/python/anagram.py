9270f9bdab128a51fa4eaa7edd9930c15d3a7462
yanndan@yahoo.com <yanndan@yahoo.com>
2021-08-19 13:10:00 +0200

    DG: component works.

--- anagram/python/anagram.py
+++ anagram/python/anagram.py
@@ -1,5 +1,6 @@
 import copy
 
+
 class Anagram():
     def __init__(self, word, word_list_path):
         self.WORD = word
@@ -8,44 +9,41 @@ class Anagram():
     def find_anagrams(self):
         wordlist = self.load_wordlist()
         letter_dict = self.decompose_word()
-        self.search_anagrams(letter_dict, wordlist)
+        return self.search_anagrams(letter_dict, wordlist)
 
     def decompose_word(self):
-        letter_dict={}
+        letter_dict = {}
         for letter in self.WORD:
             if letter not in letter_dict.keys():
-                letter_dict[letter]=1
+                letter_dict[letter] = 1
             else:
-                letter_dict[letter]+=1
+                letter_dict[letter] += 1
         return letter_dict
 
     def load_wordlist(self):
-        wordlist=[]
+        wordlist = []
         f = open(self.WORDLIST_PATH, 'r')
-        lines = f.readlines()[1:]
+        lines = f.readlines()
         for line in lines:
-            wordlist.extend(line.split())
+            if not line.startswith('#'):
+                wordlist.extend(line.split())
         return wordlist
 
-    def print_anagram(self, letter_dict, concatenated_word):
-        if self.check_words(letter_dict, concatenated_word):
-            print(concatenated_word + ' is an anagram of ' + self.WORD)
-            return True
-        return False
-        
         
     def check_words(self, letter_dict, concatenated_word):
-        other_letter_dict = copy.copy(letter_dict)
+        other_letter_dict = {}
         for letter in concatenated_word:
-            if letter not in other_letter_dict:
+            if letter not in letter_dict.keys():
                 return False
-            if not self.check_occurence(other_letter_dict, letter):
-                return False
-        return True
+            if letter in other_letter_dict.keys():
+                other_letter_dict[letter] += 1
+            else:
+                other_letter_dict[letter] = 1
+        return letter_dict == other_letter_dict
 
     def check_occurence(self, letter_dict, letter):
-        if letter_dict[letter]>0:
-            letter_dict[letter]-=1
+        if letter_dict[letter] > 0:
+            letter_dict[letter] -= 1
             return True
         return False
 
@@ -54,8 +52,9 @@ class Anagram():
         anagram_list = []
         for word1 in wordlist:
             for word2 in wordlist[index_word:]:
-                if len(word1) + len(word2) == len(self.WORD):
-                    concatenated_word = word1 + word2
-                    self.print_anagram(letter_dict, concatenated_word)
+                if len(word1) + len(word2) == len(self.WORD) and \
+                self.check_words(letter_dict, word1 + word2):
+                    print(word1 + ' & ' + word2+ ' is an anagram of ' + self.WORD)
+                    anagram_list.append(word1+word2)
             index_word += 1
-
+        return anagram_list
